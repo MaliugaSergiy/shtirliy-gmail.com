c6d99642ef775f13139bdb9fc8fba909d89722ca
Serhii <shtirliy@gmail.com>
2020-07-07 12:58:32 +0300

    fix:dsdsd

--- build/scripts.3cddadf4.js
+++ build/scripts.3cddadf4.js
@@ -4147,7 +4147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60377" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52682" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
